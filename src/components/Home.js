c0fe6b8b6bbf6f280fcd0ca1db675cc48090459c
anproagile <anproagile@gmail.com>
2021-12-01 13:52:15 -0500

    Creating functionality to load featured stories on page load.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,51 @@
 //----Necessary imports
-import React from "react";
-import "../App.css";
-import Map from "./Map";
-import Footer from "./Footer";
-import { useState } from "react"
+import React from 'react'
+import '../App.css'
+import Map from './Map'
+import Footer from './Footer'
+import { useState } from 'react'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+import Featured from './Featured'
 
 //Home function to render page structural elements
-export default function Home() {
-  const [center, setCenter] = useState([43.88, -72.7317]);
-  const [zoom, setZoom] = useState(8);
+export default function Home () {
+  const [center, setCenter] = useState([43.88, -72.7317])
+  const [zoom, setZoom] = useState(8)
+  const [featuredDisplay, setFeaturedDisplay] = useState(true)
+  const [countyStoryDisplay, setCountyStoryDisplay] = useState(false)
+  const [selectedCounty, setSelectedCounty] = useState("")
+
+  const GreenTextTypography = withStyles({
+    root: {
+      color: '#5a203c'
+    }
+  })(Typography)
+
   return (
     //React fragment (instead of <div>)
     <>
-      <div class="wrapper">
-        <div class="left">left content stuff goes here</div>
-        <main>
-          <Map
-            center={center}
-            setCenter={setCenter}
-            zoom={zoom}
-            setZoom={setZoom}
-          />{" "}
-        </main>
-        <Footer />
-      </div>
+      <Grid container spacing={2}>
+        {featuredDisplay ? <Featured /> : null}
+
+        <Grid item xs={12} sm={6} md={2}>
+          <Paper>
+            <Map
+              center={center}
+              setCenter={setCenter}
+              zoom={zoom}
+              setZoom={setZoom}
+              setFeaturedDisplay={setFeaturedDisplay}
+              setSelectedCounty={setSelectedCounty}
+              setCountyStoryDisplay={setCountyStoryDisplay}
+            />
+          </Paper>
+        </Grid>
+      </Grid>
+
+      <Footer />
     </>
-  );
+  )
 }
